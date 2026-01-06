2598d365b98caefa1bdfb05cb2e21bf662349401
Lunéa <contact.arrea@proton.me>
2026-01-06 17:09:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+ac = int(input("Entrez le nombre d'ACs: "))
+cd = int(input("Entrez le nombre de CDs: "))
+
+if ac >= (cd / 3):
+    print("L'article 5 s'applique.")
+else:
+    print("L'article 5 ne s'applique pas.")
+
+input("Appuyez sur entrer pour quitter")
